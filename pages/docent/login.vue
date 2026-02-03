d8c74f02570aeffea88b435f45e0340b56e9c201
Tom Tiedemann <tomtiedemann30@gmail.com>
2026-02-03 14:57:35 +0100

    een nieuwe page aangemaakt met een form waarin een functie word uitgevoerd als het form gesubmit word

---
+++ pages/docent/login.vue
@@ -0,0 +1,19 @@
+<script setup>
+function handleSubmit(event) {
+    const formData = new FormData(event.target)
+
+    console.log('Username:', formData.get('username'))
+    console.log('Password:', formData.get('password'))
+}
+</script>
+<template>
+    <div>
+        <form @submit.prevent="handleSubmit">
+            <label for="username">Username</label><br>
+            <input type="text" id="username" name="username" /><br>
+            <label for="password">Password</label><br>
+            <input type="password" id="password" name="password" /><br>
+            <button type="submit">Login</button>
+        </form>
+    </div>
+</template>
