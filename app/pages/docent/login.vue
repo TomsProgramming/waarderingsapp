bfafbe7064155c56d4dd133b5996c6ff1dc03f3b
liannefm <lfmeiresonne@gmail.com>
2026-02-11 09:04:39 +0100

    foto's toegevoegd en neergezet docentloginpagina

--- app/pages/docent/login.vue
+++ app/pages/docent/login.vue
@@ -1,7 +1,25 @@
 <template>
-
+    <div class="login-docenten">
+        <img src="/img/glulogo.png" class="glulogo">
+    </div>
 </template>
 
-<styling scoped>
-    
-</styling>
+<style scoped>
+.login-docenten {
+    background-image: url('/img/achtergrond-loginpagina.png');
+    background-size: cover;
+    background-position: center;
+
+    display: flex;
+    justify-content: center;
+
+    width: 100vw;
+    height: 100vh;
+}
+
+.glulogo {
+    width: 142px;
+    height: 142px;
+    margin-top: 25px;
+}
+</style>
