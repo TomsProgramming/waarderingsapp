b08a89c3446c54c9c2defb8f255b0a85e7a3e9d1
liannefm <lfmeiresonne@gmail.com>
2026-04-07 09:37:01 +0200

    css aanpassingen profiel docent

--- app/pages/docent/profiel.vue
+++ app/pages/docent/profiel.vue
@@ -8,43 +8,46 @@ const rol = "DOCENT"
 <template>
     <div class="pagina">
 
-    <div class="header">
-      <h1>PROFIEL</h1>
-      <h2>Docent</h2>
-    </div>
-    <div class="groen-vlak">
-        <div class="body">
-
-            <div class="profielfoto">
-                <img src="/img/glulogo.png" alt="Profielfoto" />
-            </div>
-
-            <div class="info-blok">
-                {{ naam }}
-            </div>
-
-            <div class="info-blok">
-                {{ rol }}
-            </div>
-
-            <div class="info-blok">
-                JAG
-            </div>
-
-            <div class="thema-blok">
-                <p>THEMA</p>
-
-                <div class="thema-opties">
-                    <div class="kleur oranje"></div>
-                    <div class="kleur wit"></div>
-                </div>
-            </div>
-
-            <button class="uitloggen">
-                UITLOGGEN
-            </button>
-
+        <div class="header">
+            <h1>PROFIEL</h1>
+            <h2>Docent</h2>
         </div>
+        <div class="groen-vlak">
+            <div class="body">
+
+                <div class="profielfoto">
+                    <img src="/img/glulogo.png" alt="Profielfoto" />
+                </div>
+
+                <div class="info-blok">
+                    {{ naam }}
+                </div>
+
+                <div class="info-blok">
+                    {{ rol }}
+                </div>
+
+                <div class="info-blok">
+                    JAG
+                </div>
+
+
+                <div id="thema-blok-outline">
+                    <div class="thema-blok">
+                        <p>THEMA</p>
+
+                        <div class="thema-opties">
+                            <div class="kleur oranje"></div>
+                            <div class="kleur wit"></div>
+                        </div>
+                    </div>
+                </div>
+
+                <button class="uitloggen">
+                    UITLOGGEN
+                </button>
+
+            </div>
         </div>
 
         <div class="footer">
@@ -56,93 +59,135 @@ const rol = "DOCENT"
 
 <style scoped>
 .pagina {
-  min-height: 100vh;
-  background-color: #f2f2f2;
-  display: flex;
-  flex-direction: column;
+    min-height: 100vh;
+    background-color: #f2f2f2;
+    display: flex;
+    flex-direction: column;
 }
 
 /* Header */
 .header {
-  padding: 25px 20px;
+    padding: 25px 20px;
 
-  font-family: "Inter", sans-serif;
+    font-family: "Inter", sans-serif;
 }
 
 .header h1 {
-  color: #1db47e;
-  font-size: 18px;
-  margin: 0;
+    color: #1db47e;
+    font-size: 18px;
+    margin: 0;
 }
 
 .header h2 {
-  color: #6ee7b7;
-  font-size: 14px;
-  margin: 0;
+    color: #6ee7b7;
+    font-size: 14px;
+    margin: 0;
 }
 
 /* GROENE CONTAINER */
 .groen-vlak {
-  flex: 1;
-  background: linear-gradient(180deg, #3ccf91, #32c48d);
-  border-top-left-radius: 30px;
-  border-top-right-radius: 30px;
-  padding-top: 40px;
+    flex: 1;
+    display: flex;
+
+    background: linear-gradient(180deg, #3ccf91, #32c48d);
+
+    border-top-left-radius: 30px;
+    border-top-right-radius: 30px;
+    
+    padding-top: 40px;
 }
 
 /* Body inhoud */
 .body {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  gap: 15px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
 
-  font-family: "Inter", sans-serif;
+    gap: 6px;
+
+    font-family: "Inter", sans-serif;
+
+    min-height: 100%;
+    width: 100%;
+}
+
+.profielfoto {
+    display: flex;
+    justify-content: center;
+
+    width: 100%;
 }
 
 .profielfoto img {
     width: 120px;
     height: 120px;
+
     border-radius: 50%;
-    object-fit: cover;
     border: 4px solid rgba(255, 255, 255, 0.4);
+
+    object-fit: cover;
 }
 
 .info-blok {
     width: 220px;
-    padding: 12px;
-    text-align: center;
+
+    padding: 14px;
+
     background-color: rgba(0, 128, 96, 0.4);
-    border-radius: 8px;
     color: white;
-    font-weight: 500;
+
+    border-radius: 8px;
+
     letter-spacing: 1px;
+    text-align: center;
+    font-size: 20px;
+}
+
+.info-blok,
+.thema-blok {
+    margin-left: auto;
+    margin-right: auto;
+}
+
+#thema-blok-outline{
+    display: flex;
+    justify-content: center;
 }
 
 .thema-blok {
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-end;
+
     width: 220px;
+
     padding: 15px;
+
     background-color: rgba(0, 128, 96, 0.4);
+
     border-radius: 8px;
+
     text-align: center;
     color: white;
 }
 
 .thema-blok p {
-    margin-bottom: 10px;
-    font-size: 12px;
+    margin-bottom: 15px;
+    font-size: 19px;
     letter-spacing: 2px;
 }
 
 .thema-opties {
     display: flex;
     justify-content: center;
+
     gap: 15px;
 }
 
 .kleur {
     width: 25px;
     height: 25px;
+
     border-radius: 8px;
 }
 
@@ -156,16 +201,22 @@ const rol = "DOCENT"
 }
 
 .uitloggen {
-    margin-top: 20px;
     width: 220px;
+
+    margin-top: auto;
+    margin-bottom: 20px;
     padding: 12px;
+
     border: none;
     border-radius: 10px;
+    cursor: pointer;
+
     background-color: rgba(0, 128, 96, 0.6);
     color: white;
-    font-weight: bold;
-    letter-spacing: 1px;
-    cursor: pointer;
+
+    font-weight: lighter;
+    letter-spacing: 2px;
+    font-size: 19px;
 }
 
 .uitloggen:hover {
